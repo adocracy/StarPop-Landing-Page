406a514e77102dda54e93831a796dba9cc678710
CHRIS NUNES <chris@holofy.com>
2015-05-17 00:19:16 -0700

    reverting to V2
    reverted code, from Parse active copy

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,7 +36,7 @@ jQuery(document).ready(function($){
 // google map inject end
 
 
-// CSS3 Animation Trigger
+// CSS3 Amination Trigger
 
 //all effects class list
 var effects = ['flipInX', 'flipInY', 'fadeIn', 'fadeInUp', 'fadeInDown', 'fadeInLeft', 'fadeInRight', 'fadeInUpBig', 'fadeInDownBig', 'fadeInLeftBig', 'fadeInRightBig', 'slideInDown', 'slideInLeft', 'slideInRight', 'bounceIn', 'bounceInUp', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'rotateIn', 'rotateInUpLeft', 'rotateInDownLeft', 'rotateInUpRight', 'rotateInDownRight', 'lightSpeedIn', 'rollIn'];
@@ -69,14 +69,14 @@ jQuery(window).scroll(function(event){
 // CSS3 Amination Trigger
 
 
-// smoothscroll
+// smothscroll
         $('.scroll').on('click',function (e) {
             e.preventDefault();
             $('html, body').stop().animate({
                 'scrollTop': $($(this).attr('href')).offset().top
             }, 500, 'swing', function () {});
         });
-// smoothscroll
+// smothscroll
 
 //nicescroll start
 
@@ -96,4 +96,89 @@ jQuery(window).scroll(function(event){
         //smoothscroll : false
         hidecursordelay : 150
     });
+
+
+    // contact form start
+    $('#contact').submit(function(){
+        var fromData = $('#contact').serialize();
+        // var confirmMSG = '<div id="contact-confirm" class="animated fadeIn"><div class="confirm-inner animated fadeInUp">Your message sent successfully <a id="closebtn" class="btn btn-warning btn-lg btn-block">Close</a></div></div>';
+
+        $(document.body).on('click', '#closebtn', function(){
+            $('#contact-confirm').remove();
+        });
+
+        $.ajax({
+            type: "POST",
+            url: "contact.php",
+            data: fromData,
+            success : function( data ) {
+                // $('.contact-form-wrapper').append(confirmMSG);
+                // $(confirmMSG).insertAfter('#footer');
+                alert('Your Message Sent successfully');
+            },
+            error   : function( xhr, err ) {
+                         alert(fromData);     
+            }
+        });
+
+        return false;
+    });
+    // contact form end
+})
+
+
+
+// google map start
+
+google.maps.event.addDomListener(window, 'load', init);
+
+var map;
+
+function init() {
+    var mapOptions = {
+        center: new google.maps.LatLng(23.825527, 90.429862),
+        zoom: 15,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.DEFAULT,
+        },
+        disableDoubleClickZoom: false,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        },
+        scaleControl: true,
+        scrollwheel: false,
+        streetViewControl: false,
+        draggable : true,
+        overviewMapControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [ 
+    { featureType: "administrative", elementType: "all", stylers: [ { visibility: "on" }, { saturation: -100 }, { lightness: 20 } ] },
+    { featureType: "road", elementType: "all", stylers: [ { visibility: "on" }, { saturation: -100 }, { lightness: 40 } ] },
+    { featureType: "water", elementType: "all", stylers: [ { visibility: "on" }, { saturation: -10 }, { lightness: 30 } ] },
+    { featureType: "landscape.man_made", elementType: "all", stylers: [ { visibility: "simplified" }, { saturation: -60 }, { lightness: 10 } ] },
+    { featureType: "landscape.natural", elementType: "all", stylers: [ { visibility: "simplified" }, { saturation: -60 }, { lightness: 60 } ] },
+    { featureType: "poi", elementType: "all", stylers: [ { visibility: "off" }, { saturation: -100 }, { lightness: 60 } ] }, 
+    { featureType: "transit", elementType: "all", stylers: [ { visibility: "off" }, { saturation: -100 }, { lightness: 60 } ] }
+],
+    
+    }
+
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var locations = [
+        ['', 23.825527, 90.429862]
+    ];
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            icon: 'assets/img/map-marker.png',
+            animation: google.maps.Animation.BOUNCE,
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+        });
+    }
+    // google map end
+}
 
